d37ef98908e03bf1593365ed2eb973fd4aaac63b
Talipov S.N. <proffix4@gmail.com>
2025-01-26 15:51:58 +0500

    CppPrWorkDemos

---
+++ CppPrWork1_1_demo/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS // отключение предупреждений о небезопасности
+
+#include<iostream> // подключение библиотеки iostream для использования функций ввода/вывода
+#include<math.h> // подключение библиотеки math.h для использования математических функций
+
+int main() {
+	setlocale(LC_ALL, "Russian"); // установка русской локализации
+
+	float x, y, b;
+	printf("Введите исходные данные х, b через пробел : ");
+	scanf("%f %f", &x, &b);
+
+	if (x < 10) {
+		y = 1 + b * pow(x, 2);
+	} else {
+		if (x <= 15) {
+			y = log(x) + b;
+		} else {
+			y = sin(b * x);
+		}
+	}
+
+	printf("y = %7.3f", y);
+}
---
+++ CppPrWork1_3_demo/func.h
@@ -0,0 +1,7 @@
+#ifndef FUNC_H // include guard
+#define FUNC_H // include guard
+
+float func(float z, float a, float x); // объявление функции
+
+#endif /* FUNC_H */ // include guard
+
